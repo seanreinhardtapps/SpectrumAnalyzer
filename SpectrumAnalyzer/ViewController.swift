0d627ad02aed75ba7b6ff85d75b60341bf656d45
Sean Reinhardt <seanreinhardt@Seans-Air.lan>
2022-06-12 13:28:45 -0700

    base project

---
+++ SpectrumAnalyzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpectrumAnalyzer
+//
+//  Created by Sean Reinhardt on 6/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
