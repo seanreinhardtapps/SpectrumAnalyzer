1a9dcde4a4ddba659f4b9f60c79aa94d7a578e58
Sean Reinhardt <seanreinhardtapps@gmail.com>
2022-09-23 12:35:46 -0700

    update delegate protocol to return FrequencyResponse struct

--- AudioSpectrumKit/AudioSpectrumKit.swift
+++ AudioSpectrumKit/AudioSpectrumKit.swift
@@ -9,13 +9,14 @@ import UIKit
 import AVFoundation
 private let microphonePermissionKey = "NSMicrophoneUsageDescription"
 
-@objc public protocol AudioSpectrumKitResultsDelegate {
-    @objc optional func didReceiveSample(data:[[String:Double]])
-    @objc optional func samplingFailedToStart(error:Error)
-    @objc optional func samplingFailedToSetup(error:Error)
+public protocol AudioSpectrumKitResultsDelegate: NSObject {
+    func didReceiveSample(frequencyResponse:FrequencyResponse)
+    func samplingFailedToStart(error:Error)
+    func samplingFailedToSetup(error:Error)
 }
 
-public class AudioSpectrumKit: NSObject {
+public class AudioSpectrumKit: NSObject, AudioSamplingInstanceDelegate {
+    
     var delegate: AudioSpectrumKitResultsDelegate?
     internal var audioSession: AVAudioSession {
         didSet {
@@ -23,7 +24,7 @@ public class AudioSpectrumKit: NSObject {
                 try audioSession.setCategory(.record)
                 try audioSession.setMode(AVAudioSession.Mode.measurement)
             } catch {
-                delegate?.samplingFailedToSetup?(error: error)
+                delegate?.samplingFailedToSetup(error: error)
             }
         }
     }
@@ -37,11 +38,11 @@ public class AudioSpectrumKit: NSObject {
         super.init()
         
         if !verifyRecordingPermissionKey(bundle: bundle) {
-            delegate?.samplingFailedToSetup?(error: AudioSpectrumKitErrorFactory.plistError())
+            delegate?.samplingFailedToSetup(error: AudioSpectrumKitErrorFactory.plistError())
             return
         }
         
-        self.samplingInstance = SamplingInstance.init(audioSession: self.audioSession)
+        self.samplingInstance = SamplingInstance.init(delegate: self, audioSession: self.audioSession)
     }
     
     func verifyRecordingPermissionKey(bundle:Bundle) -> Bool {
@@ -57,7 +58,7 @@ public class AudioSpectrumKit: NSObject {
             if !granted {
                 OperationQueue.main.addOperation {
                     if let strongSelf = weakSelf {
-                        strongSelf.delegate?.samplingFailedToSetup?(error: AudioSpectrumKitErrorFactory.permissionError())
+                        strongSelf.delegate?.samplingFailedToSetup(error: AudioSpectrumKitErrorFactory.permissionError())
                     }
                 }
                 return
@@ -66,5 +67,15 @@ public class AudioSpectrumKit: NSObject {
                 instance.sample()
             }
         }
+    }
+    
+    func sampleProcessed(result: [Float]) {
+        var data: [Frequency] = []
+        for (index, value) in result.enumerated() {
+            if index % 100 == 0 {
+                data.append(Frequency(id: "\(index)Hz", db: value))
+            }
+        }
+        delegate?.didReceiveSample(frequencyResponse: FrequencyResponse(response: data))
     }
 }
